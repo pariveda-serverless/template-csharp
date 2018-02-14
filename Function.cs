e6203eb038d62bee5878e9ba09b5d91617abcb82
jackwarner <jack.warner@parivedasolutions.com>
2018-02-14 16:28:18 -0500

    Working deploy to Lambda without MH

---
+++ Function.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace nettest
+{
+    
+    public class Functions
+    {
+       public Response Hello(Request request, ILambdaContext context)
+       {
+           return new Response("Go Serverless .NET Core 2! Your function executed successfully!", request);
+       }
+
+    }
+
+    public class Response
+    {
+      public string Message {get; set;}
+      public Request Request {get; set;}
+
+      public Response(string message, Request request){
+        Message = message;
+        Request = request;
+      }
+    }
+
+    public class Request
+    {
+      public string Key1 {get; set;}
+      public string Key2 {get; set;}
+      public string Key3 {get; set;}
+
+      public Request(string key1, string key2, string key3){
+        Key1 = key1;
+        Key2 = key2;
+        Key3 = key3;
+      }
+    }
+}
